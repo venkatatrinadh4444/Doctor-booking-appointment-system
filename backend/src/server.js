ae3307dacde1d7a4683f32ed78a8a1315efeed2d
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-04-21 18:04:42 +0530

    updated origin in backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const appointmentRoutes=require('./routes/appointmentRoutes')
 const app=express()
 const PORT=process.env.PORT || 5000
 
-app.use(cors({origin:["http://localhost:5174","http://localhost:5173"],credentials:true}))
+app.use(cors({origin:["https://doctor-booking-appointment-system-taupe.vercel.app","https://doctor-booking-appointment-system-p8am.vercel.app"],credentials:true}))
 app.use(cookieParser())
 app.use(express.json())
 
